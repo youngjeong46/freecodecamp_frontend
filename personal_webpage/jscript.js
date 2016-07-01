8161d51aafb3cd8b3376b3445c70fecebab6e3b8
Young Jeong <youngjeong46@gmail.com>
2016-07-01 13:16:29 -0700

    personal page completed

--- personal_webpage/jscript.js
+++ personal_webpage/jscript.js
@@ -1,10 +1,25 @@
-$(document).ready(function(){
-	$('body').scrollspy({target: ".navbar", offset: 50}); 
+$(document).ready(function() {
+  $('body').scrollspy({
+    target: ".navbar",
+    offset: 50
+  });
 
-	// Add smooth scrolling to all links inside a navbar
-	$(".nav a").on('click', function(event){
+  // Add smooth scrolling to all links inside a navbar
+  $(".nav a").on('click', function(event) {
 
     $(".nav").find(".active").removeClass("active");
     $(this).parent().addClass("active");
+    
+    var target;
+    target = this.hash;
+
+    event.preventDefault();
+
+    return $('html, body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 600, function() {
+      return window.history.pushState(null, null, target);
+    });
   });
+
 });
